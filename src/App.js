5201ce68960b9e43a35126f260e648f64abc3de9
Julieta Lazzaro <julietalazzaro@gmail.com>
2020-09-05 11:49:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <h1>Noticias</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Icons made by
+          <a href="http://www.freepik.com/">Freepik</a>
+          from
+          <a href="https://www.flaticon.com/">www.flaticon.com</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
